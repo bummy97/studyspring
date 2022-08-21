b89411eb2870798e228a57de7d4b269987e3430e
bummy <dydqja4582@naver.com>
2022-08-22 00:30:09 +0900

    index.js 파일 만들기 수정 기능을 위한 수정 페이지 만들기

--- src/main/java/com/ybkim/springstudy/springboot/service/PostsService.java
+++ src/main/java/com/ybkim/springstudy/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.ybkim.springstudy.springboot.service;
 
 import com.ybkim.springstudy.springboot.domain.posts.Posts;
 import com.ybkim.springstudy.springboot.domain.posts.PostsRepository;
+import com.ybkim.springstudy.springboot.web.dto.PostsListResponseDto;
 import com.ybkim.springstudy.springboot.web.dto.PostsReponseDto;
 import com.ybkim.springstudy.springboot.web.dto.PostsSaveRequestDto;
 import com.ybkim.springstudy.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -34,5 +38,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return  new PostsReponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
