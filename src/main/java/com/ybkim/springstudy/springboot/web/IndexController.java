f802269553f00dd70b31beedc20cdf4618510221
bummy <dydqja4582@naver.com>
2022-08-28 23:28:49 +0900

    구글 로그인 연동하기

--- src/main/java/com/ybkim/springstudy/springboot/web/IndexController.java
+++ src/main/java/com/ybkim/springstudy/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.ybkim.springstudy.springboot.web;
 
+import com.ybkim.springstudy.springboot.config.auth.dto.SessionUser;
 import com.ybkim.springstudy.springboot.service.PostsService;
 import com.ybkim.springstudy.springboot.web.dto.PostsReponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
     @GetMapping("/")
     public String index(Model model){ //Model: 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다.
         //여기서는 postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달한다.
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
